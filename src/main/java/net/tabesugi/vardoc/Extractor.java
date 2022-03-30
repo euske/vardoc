fa6f0be0df504bc624fa3d81a89363ca31caaa05
Yusuke Shinyama <yusuke@shinyama.jp>
2022-03-30 20:44:05 +0900

    Added: directory walking support.

--- src/main/java/net/tabesugi/vardoc/Extractor.java
+++ src/main/java/net/tabesugi/vardoc/Extractor.java
@@ -2,6 +2,7 @@ package net.tabesugi.vardoc;
 
 import java.io.*;
 import java.nio.file.*;
+import java.nio.file.attribute.*;
 import java.util.*;
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseResult;
@@ -16,30 +17,46 @@ import com.github.javaparser.utils.SourceRoot;
 
 public class Extractor {
 
+    public static void doFile(Path path) throws IOException {
+        JavaParser parser = new JavaParser();
+        ParseResult<CompilationUnit> result = parser.parse(path);
+        if (!result.isSuccessful()) return;
+        CompilationUnit cu = result.getResult().get();
+        cu.accept(new VoidVisitorAdapter<Void>() {
+                @Override
+                public void visit(MethodDeclaration decl, Void arg) {
+                    Optional<Javadoc> javadoc = decl.getJavadoc();
+                    if (javadoc.isPresent()) {
+                        Range range = decl.getRange().get();
+                        System.out.println(range);
+                        for (JavadocBlockTag tag : javadoc.get().getBlockTags()) {
+                            System.out.println(tag);
+                        }
+                    }
+                }
+            }, null);
+    }
+
     public static void main(String[] args) throws IOException {
         Log.setAdapter(new Log.StandardOutStandardErrorAdapter());
 
-        JavaParser parser = new JavaParser();
+        PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:*.java");
         for (String arg1 : args) {
             Path path = Paths.get(arg1);
-            ParseResult<CompilationUnit> result = parser.parse(path);
-            if (result.isSuccessful()) {
-                CompilationUnit cu = result.getResult().get();
-                cu.accept(new VoidVisitorAdapter<Void>() {
-                        @Override
-                        public void visit(MethodDeclaration decl, Void arg) {
-                            Optional<Javadoc> javadoc = decl.getJavadoc();
-                            if (javadoc.isPresent()) {
-                                Range range = decl.getRange().get();
-                                System.out.println(range);
-                                for (JavadocBlockTag tag : javadoc.get().getBlockTags()) {
-                                    System.out.println(tag);
-                                }
+            Files.walkFileTree(
+                path,
+                new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                        if (matcher.matches(file.getFileName())) {
+                            try {
+                                doFile(file);
+                            } catch (IOException e) {
                             }
                         }
-                    }, null);
-
-            }
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
         }
     }
 }
