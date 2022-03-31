e13fcb992be0d4f00391ade8b5f89c2cc8a799e2
Yusuke Shinyama <yusuke@shinyama.jp>
2022-03-31 09:39:15 +0900

    Code cleanup and field support.

--- src/main/java/net/tabesugi/vardoc/Extractor.java
+++ src/main/java/net/tabesugi/vardoc/Extractor.java
@@ -1,45 +1,62 @@
+//  Extractor.java
+//
 package net.tabesugi.vardoc;
 
 import java.io.*;
 import java.nio.file.*;
 import java.nio.file.attribute.*;
 import java.util.*;
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ParseResult;
-import com.github.javaparser.Range;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import com.github.javaparser.javadoc.Javadoc;
-import com.github.javaparser.javadoc.JavadocBlockTag;
-import com.github.javaparser.utils.Log;
-import com.github.javaparser.utils.SourceRoot;
+import com.github.javaparser.*;
+import com.github.javaparser.ast.*;
+import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.visitor.*;
+import com.github.javaparser.javadoc.*;
+import com.github.javaparser.javadoc.description.*;
+import com.github.javaparser.utils.*;
 
 public class Extractor {
 
-    public static void doFile(Path path) throws IOException {
-        JavaParser parser = new JavaParser();
+    private JavaParser parser = new JavaParser();
+
+    public void doFile(Path path) throws IOException {
         ParseResult<CompilationUnit> result = parser.parse(path);
         if (!result.isSuccessful()) return;
         CompilationUnit cu = result.getResult().get();
-        cu.accept(new VoidVisitorAdapter<Void>() {
+        cu.accept(
+            new VoidVisitorAdapter<Void>() {
                 @Override
                 public void visit(MethodDeclaration decl, Void arg) {
                     Optional<Javadoc> javadoc = decl.getJavadoc();
-                    if (javadoc.isPresent()) {
-                        Range range = decl.getRange().get();
-                        System.out.println(range);
-                        for (JavadocBlockTag tag : javadoc.get().getBlockTags()) {
-                            System.out.println(tag);
-                        }
+                    if (!javadoc.isPresent()) return;
+                    Range range = decl.getRange().get();
+                    for (JavadocBlockTag tag : javadoc.get().getBlockTags()) {
+                        show(range, tag);
                     }
                 }
+                @Override
+                public void visit(FieldDeclaration decl, Void arg) {
+                    Optional<Javadoc> javadoc = decl.getJavadoc();
+                    if (!javadoc.isPresent()) return;
+                    Range range = decl.getRange().get();
+                    showField(range, javadoc.get().getDescription());
+                }
             }, null);
+    }
+
+    public void show(Range range, JavadocBlockTag tag) {
+        System.out.println(range);
+        System.out.println(tag);
+    }
+
+    public void showField(Range range, JavadocDescription desc) {
+        System.out.println(range);
+        System.out.println(desc);
     }
 
     public static void main(String[] args) throws IOException {
         Log.setAdapter(new Log.StandardOutStandardErrorAdapter());
 
+        Extractor extractor = new Extractor();
         PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:*.java");
         for (String arg1 : args) {
             Path path = Paths.get(arg1);
@@ -47,10 +64,11 @@ public class Extractor {
                 path,
                 new SimpleFileVisitor<Path>() {
                     @Override
-                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                    public FileVisitResult visitFile(
+                        Path file, BasicFileAttributes attrs) {
                         if (matcher.matches(file.getFileName())) {
                             try {
-                                doFile(file);
+                                extractor.doFile(file);
                             } catch (IOException e) {
                             }
                         }
--- src/test/resources/Foo.java
+++ src/test/resources/Foo.java
@@ -7,10 +7,15 @@ public class Foo {
      *
      * @param a some stringhy!
      * @param xxx whoa, whoa!
-     *        what a hack?
+     *        what a <b>hack</b>?
      * @return boo.
      */
     public int moo(String a, int xxx) {
         return 123;
     }
+
+    /**
+     * some value.
+     */
+    public int value;
 }
